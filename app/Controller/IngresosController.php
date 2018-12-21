e8f7cd2331028f117438ca0bdf0498d250ac1cf3
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2018-12-20 20:33:51 -0600

    formulario ingreso, validacion campos

--- app/Controller/IngresosController.php
+++ app/Controller/IngresosController.php
@@ -1,9 +1,27 @@
 <?php
-namespace App\Controller;
+require_once "../../vendor/autoload.php";
+use Respect\Validation\Validator as v;
 
-use App\Model\Ingresos;
+// guarda ingresos
+if (isset($_POST['save'])) {
+    $data = $_POST; //  datos enviados 
+    //  reglas de validacion
+    $description = v::stringType()->notEmpty();
+    $monto = v::numeric()->notEmpty();
+    $ticket = v::numeric()->notEmpty();
+   
+    try {
+        //  se validan los datos
+        $description->assert($data['description']);
+        $ticket->assert($data['ticket']);
+        $monto->assert($data['mount']);
+        //  guardado BD
 
-class IngresosController
-{
-    
+        //  respuesta de guardado
+        echo "<div class='alert alert-success'>Guardado</div>";
+    } catch (\Exception $e) {
+        //  Si falla la validacion
+        echo "<div class='alert alert-danger'>Todos los campos son requeridos</div>";
+    }
+
 }
--- views/registraIngreso.php
+++ views/registraIngreso.php
@@ -1,6 +1,51 @@
 <?php
-use App\Controller\IngresosController;
+require_once "../content/header.html";
+?>
 
+<div class = "container">
+    <hr>
+    <h3>Registro de Ingresos</h3>
+    <div class = "row">
+        <div class = 'col'>
+            <form id = 'registraIngresos'>
+                <div class = "form-group">
+                    <label for="description">Descripción</label>
+                    <input type="text" class='form-control' id = 'description' name='description'>
+                </div>
+                <div class = "form-group">
+                    <label for="ticket"># Ticket</label>
+                    <input type="number" class='form-control' id = 'ticket' name='ticket'>
+                </div>
+                <div class = "form-group">
+                    <label for="mount">Monto</label>
+                    <input type="bumber" step = 'any' class='form-control' id = 'mount' name='mount'>
+                </div>
+                <span id = 'responseText'></span>
+                <button type='button' class='btn btn-success btn-sm btn-block' id = 'saveIngreso' name = 'saveIngreso'> Registrar</button>
+            </form>
+        </div>
+        <div class = 'col'></div>
+    </div>
+</div>
+<script>
+let btn_guarda = document.querySelector("#saveIngreso");
+    btn_guarda.addEventListener("click", function () {
+        let url = "../app/Controller/IngresosController.php";
+        let form = document.querySelector("#registraIngresos");
+        let formData = new FormData(form);
+        formData.append("save", true);
+        let request = new XMLHttpRequest();
+        request.open('POST', url, true);
+        //  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        request.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.querySelector("#responseText").innerHTML = this.responseText;
+            }
+        }
+        request.send(formData);
+    });
 
-$msg = new IngresosController();
-$msg->getDBName();
+</script>
+<?php
+require_once "../content/footer.html";
+?>
