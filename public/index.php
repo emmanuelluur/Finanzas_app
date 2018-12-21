004ad10b7b546af5227a43e95176fb15e20bc49c
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2018-12-21 12:46:54 -0600

    htaccess config

--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
 <?php
 include_once "../vendor/autoload.php";
 
-$route = $_GET['route'] ?? '/';
+$route = $_GET['route'] ?? 'inicio';
 
 $protected = "all";
 
-if($route == '/'):
+if($route == 'inicio'):
     require_once "../views/index.php";
 elseif(file_exists("../views/{$route}.php")):
     require_once "../views/{$route}.php";
 else:
-    echo "Se perdio este archivo...";
+    require_once "../views/404.php";
 endif;
---
+++ views/404.php
@@ -0,0 +1,19 @@
+<?php
+require_once "../content/header.html";
+?>
+
+<div class = "container">
+    <hr>
+    
+    <div class = "row">
+        <div class = 'col-sm-2'></div>
+        <div class = 'col-sm-4'>
+            <p><h4>Se perdio este archivo...</h4></p>
+            <img src="../uploads/404.jpg" alt="404">
+        </div>
+        
+    </div>
+</div>
+<?php
+require_once "../content/footer.html";
+?>
