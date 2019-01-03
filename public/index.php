5824c82b83c260ebd69def218d09036e73a97eb9
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2019-01-02 19:29:01 -0600

    ruta

--- public/index.php
+++ public/index.php
@@ -2,33 +2,64 @@
 include_once "../vendor/autoload.php";
 session_start();
 
-$map = [
-    "ruta" =>
-    [
-        "ingresos" => ["file" => "../views/registraIngreso.php"],
-        "gastos" => ["file" => "../views/registraGasto.php"],
-        "usuario" => ["file" => "../views/usuario.php"],
-        "login" => ["file" => "../views/login.php"],
-        "logout" => ["file" => "../views/logout.php"],
-        "edita-usuario" => ["file" => "../views/usuarioEdita.php"],
-        "categorias" => ["file" => "../views/registraCategoria.php"]
-    ],
+$map = [];
 
-];
+if (!isset($_SESSION['idUser'])) {
+    $map = [
+        "ruta" =>
+        [
+            "test" => ["file" => "../views/test.php"],
+            "ingresos" => ["file" => "../views/login.php"],
+            "gastos" => ["file" => "../views/login.php"],
+            "usuario" => ["file" => "../views/usuario.php"],
+            "login" => ["file" => "../views/login.php"],
+            "logout" => ["file" => "../views/logout.php"],
+            "edita-usuario" => ["file" => "../views/login.php"],
+            "categorias" => ["file" => "../views/login.php"],
+            "" => ["file" => "../views/login.php"],
+            "inicio" => ["file" => "../views/login.php"]
+            
+        ],
+    ];
+     //  $id = $_SESSION['idUser'];
+     $route = $_GET['route'] ?? 'inicio';
+
+     if ($route == 'inicio'):
+         @require_once "../views/login.php";
+     elseif (@ file_exists($map['ruta'][$_GET['route']]['file'])):
+         require_once $map['ruta'][$_GET['route']]['file'];
+     else:
+         require_once "../views/404.php";
+     endif;
+} else {
+    $map = [
+        "ruta" =>
+        [
+            "inicio" => ["file" => "../views/index.php"],
+            "test" => ["file" => "../views/test.php"],
+            "ingresos" => ["file" => "../views/registraIngreso.php"],
+            "gastos" => ["file" => "../views/registraGasto.php"],
+            "usuario" => ["file" => "../views/usuario.php"],
+            "login" => ["file" => "../views/login.php"],
+            "logout" => ["file" => "../views/logout.php"],
+            "edita-usuario" => ["file" => "../views/usuarioEdita.php"],
+            "categorias" => ["file" => "../views/registraCategoria.php"]
+        ],
+    
+    ];
+     //  $id = $_SESSION['idUser'];
+     $route = $_GET['route'] ?? 'inicio';
+
+     if ($route == 'inicio'):
+         @require_once "../views/index.php";
+     elseif (@ file_exists($map['ruta'][$_GET['route']]['file'])):
+         require_once $map['ruta'][$_GET['route']]['file'];
+     else:
+         require_once "../views/404.php";
+     endif;
+}
 
 //require_once ($map['ruta'][$_GET['route']]['file']);die;
 
-if (!isset($_SESSION['idUser'])) {
-    require_once "../views/login.php";
-} else {
-    $id = $_SESSION['idUser'];
-    $route = $_GET['route'] ?? 'inicio';
 
-    if ($route == 'inicio'):
-        require_once "../views/index.php";
-    elseif (@ file_exists($map['ruta'][$_GET['route']]['file'])):
-        require_once $map['ruta'][$_GET['route']]['file'];
-    else:
-        require_once "../views/404.php";
-    endif;
-}
+   
---
+++ views/test.php
@@ -0,0 +1,2 @@
+<?php
+echo "prueba ruta no protegida";
